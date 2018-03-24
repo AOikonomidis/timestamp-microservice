f6868a1b17d2d4f116f3f7dc89abcfe566b477ff
Andrew Oikonomidis <andyreneyload@gmail.com>
2018-03-24 04:01:27 +0200

    add GET /:date

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const fs = require('fs');
 const path = require('path');
 
@@ -17,6 +18,26 @@ app.get('/', (req, res) => {
             console.log(`Sent: ${fileName}`);
         }
     });
+});
+
+app.get('/:date', (req, res) => {
+    let date;
+
+    if (/^\d{8,}$/.test(req.params.date)) {
+        date = moment(req.params.date, 'X');
+    } else {
+        date = moment(req.params.date, 'MMMM DD, YYYY');
+    }
+
+    let info = date.isValid() ? {
+        unix: date.format('X'),
+        natural: date.format('MMMM DD, YYYY')
+    } : {
+        unix: null,
+        natural: null
+    };
+
+    res.send(info);
 });
 
 app.listen(port, () => console.log(`Started on port ${port}`));
